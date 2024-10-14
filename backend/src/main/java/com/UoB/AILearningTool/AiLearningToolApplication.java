003d7fdfa20b95b23454a88221688e2b1aaaa7d1
Vlad <vladikkir4@gmail.com>
2024-10-14 23:52:17 +0100

    Add initial Spring Boot structure.
    Add initial Spring Boot backend structure, in 'backend' directory.

---
+++ backend/src/main/java/com/UoB/AILearningTool/AiLearningToolApplication.java
@@ -0,0 +1,13 @@
+package com.UoB.AILearningTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearningToolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/UoB/AILearningTool/AiLearningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UoB.AILearningTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLearningToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
