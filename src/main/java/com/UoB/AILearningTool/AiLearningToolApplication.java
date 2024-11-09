84af9ec051e423a1929591272f5b6a65c985ebbb
RainBOY-ZZX <kh23199@bristol.ac.uk>
2024-11-09 11:58:54 +0000

    Update AiLearningToolApplication.java

--- src/main/java/com/UoB/AILearningTool/AiLearningToolApplication.java
+++ src/main/java/com/UoB/AILearningTool/AiLearningToolApplication.java
@@ -3,7 +3,7 @@ package com.UoB.AILearningTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
 public class AiLearningToolApplication {
 
 	public static void main(String[] args) {
