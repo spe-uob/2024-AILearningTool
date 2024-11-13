47b3803761931b6ecf33d1d55ad618e0c0a48563
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2024-11-13 10:57:25 +0000

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app');
+import Cookie from './Display interface/Cookie.vue';
+import MainView from './Display interface/MainView.vue'; // 包含主界面所有部分
+
+const routes = [
+    { path: '/', component: Cookie }, // 初始路径加载 Cookie 页面
+    { path: '/main', component: MainView }, // 主界面
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
