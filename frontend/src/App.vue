3714314bc4da27a63a53da1b694f73e7fe108264
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2024-11-13 10:57:44 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,24 @@
 <template>
   <div id="app">
-    <!-- If user is not logged in, show the login screen -->
-    <Login v-if="!isLoggedIn" @login-success="handleLoginSuccess" />
-
-    <!-- If user is logged in, show the main content -->
-    <div v-else class="app-container">
-      <div class="left-sidebar-container">
-        <HistorySidebar />
-        <SettingSidebar @toggleSettings="toggleSettings" />
-      </div>
-      <MainContent />
-      <ImportantSidebar :isDisabled="isSettingsOpen" />
-    </div>
+    <!-- 路由视图 -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import Login from './components/Login.vue';
-import HistorySidebar from './components/HistorySidebar.vue';
-import MainContent from './components/MainContent.vue';
-import ImportantSidebar from './components/ImportantSidebar.vue';
-import SettingSidebar from './components/SettingSidebar.vue';
-
 export default {
-  data() {
-    return {
-      isLoggedIn: false,  // Controls login state
-      isSettingsOpen: false,  // Controls if settings are open
-    };
-  },
-  methods: {
-    handleLoginSuccess() {
-      this.isLoggedIn = true;  // Set to true on successful login
-    },
-    toggleSettings(isOpen) {
-      this.isSettingsOpen = isOpen;
-    },
-  },
-  components: {
-    Login,
-    HistorySidebar,
-    MainContent,
-    ImportantSidebar,
-    SettingSidebar,
-  },
+  name: 'App',
 };
 </script>
 
 <style>
-.app-container {
-  display: flex;
-  height: 100vh;
-}
-
-.left-sidebar-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100vh;
+/* 添加全局样式 */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
 }
 </style>
