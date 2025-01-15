6220d8ef4e5f4c08c3b9fc480ecbaa4b59557401
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2025-01-15 10:25:22 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const MainView = () => import('./Display interface/MainView.vue');
 const routes = [
     { path: '/', component: Cookie, meta: { title: 'Cookie' } },
     { path: '/main', component: MainView, meta: { title: 'Chatbot' } },
-    // 移除了 404 页面配置
+
 ];
 
 const router = createRouter({
