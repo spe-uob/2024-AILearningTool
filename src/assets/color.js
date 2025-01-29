ae3217739702948c6ff11d3b4119aaac8bdfdd82
Gerard Chaba <tl23383@bristol.ac.uk>
2025-01-29 18:01:49 +0000

    feature: update color.js
    Updated defauly background color.

--- src/assets/color.js
+++ src/assets/color.js
@@ -3,7 +3,7 @@ const colorSchemes = {
         primary: "#5C88DA",  // IBM Blue
         secondary: "#FFFFE0",  // Light Yellow
         accent: "#CDDAF3",  // Pastel Blue
-        background: "#E0E0E0",  // Light Gray
+        background: "#F4F4F4",  // Light Gray
         text: "#2E2E2E",  // Dark Gray
         border: "#D3D3D3",  // Light Border Gray
         button: "#5C88DA",  // IBM Blue
