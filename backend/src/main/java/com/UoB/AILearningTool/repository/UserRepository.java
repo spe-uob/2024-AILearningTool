b8fe668672e6e68c4feba8c3b67274749adb8670
SiyuanZhang <gr23994@bristol.ac.uk>
2025-02-17 14:04:14 +0000

    Create UserRepository.java

---
+++ backend/src/main/java/com/UoB/AILearningTool/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.UoB.AILearningTool.repository;
+
+import com.UoB.AILearningTool.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByUsername(String username);
+}
