202599f0e229ee86d944fc4791376cc09ca0cb38
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2025-02-22 20:22:39 +0000

    Update UserTest.java

--- backend/src/test/java/com/UoB/AILearningTool/UserTest.java
+++ backend/src/test/java/com/UoB/AILearningTool/UserTest.java
@@ -5,18 +5,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
+
     @Test
     @DisplayName("Checking whether 'User' constructor creates an object with optional consent correctly")
     public void correctFullConsentUserCreationTest() {
-        User user = new User(true);
-        Assertions.assertEquals(25, user.getID().length());
+        User user = new User("testUser", true); 
+        Assertions.assertEquals("testUser", user.getID());  
         Assertions.assertTrue(user.getOptionalConsent());
     }
+
     @Test
     @DisplayName("Checking whether 'User' constructor creates an object without optional consent correctly")
     public void correctRequiredConsentUserCreationTest() {
-        User user = new User(false);
-        Assertions.assertEquals(25, user.getID().length());
+        User user = new User("testUser2", false);  
+        Assertions.assertEquals("testUser2", user.getID()); 
         Assertions.assertFalse(user.getOptionalConsent());
     }
 }
