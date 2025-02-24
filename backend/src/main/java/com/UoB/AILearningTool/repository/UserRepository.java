ddfa48c18288d4797264c18fc811f797ba354a90
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2025-02-24 19:20:00 +0000

    Update UserRepository.java

--- backend/src/main/java/com/UoB/AILearningTool/repository/UserRepository.java
+++ backend/src/main/java/com/UoB/AILearningTool/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.UoB.AILearningTool.repository;
 
-import com.UoB.AILearningTool.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.UoB.AILearningTool.model.UserEntity;
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username); 
 }
