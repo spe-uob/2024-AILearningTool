57b43484805fa9bc26f0614c5e5243228143f79f
WeifanLiu2270269 <123551283+Liuwf4319@users.noreply.github.com>
2025-02-24 19:20:21 +0000

    Update ChatRepository.java

--- backend/src/main/java/com/UoB/AILearningTool/repository/ChatRepository.java
+++ backend/src/main/java/com/UoB/AILearningTool/repository/ChatRepository.java
@@ -1,9 +1,14 @@
 package com.UoB.AILearningTool.repository;
 
 import com.UoB.AILearningTool.model.ChatEntity;
+import com.UoB.AILearningTool.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<ChatEntity, String> {
+
+    List<ChatEntity> findByOwner(UserEntity owner);
 }
